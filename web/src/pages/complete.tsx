a95ced1bce0de78e6a9649db01b8befafbb0285b
Bit-Barron <1bit.baron@gmail.com>
2023-01-01 20:05:51 +0100

    complete finished

--- web/src/pages/complete.tsx
+++ web/src/pages/complete.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Button from '../components/elements/Button';
-import Input from '../components/elements/Input';
 
 interface completeProps {}
 
 const complete: React.FC<completeProps> = ({}) => {
   return (
     <section className='bg-[#181a1b] container mx-auto mt-52 p-20 w-1/2'>
-      <div className='flex flex-wrap mb-6 mx-auto'>
+      <div className='font-bold flex text-lg ml-56 mb-10'>Please complete your profile to be able to use LetterXpress to its full potential.</div>
+      <form className='flex flex-wrap mb-6 mx-auto'>
         <div className='w-96 px-3 ml-52'>
           <label
             className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
@@ -32,7 +32,7 @@ const complete: React.FC<completeProps> = ({}) => {
             id='grid-password'
           />
         </div>
-      </div>
+      </form>
       <div className='flex flex-wrap mb-6 mx-auto'>
         <div className='w-96 px-3 ml-52'>
           <label
