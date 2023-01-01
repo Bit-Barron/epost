58ccd235070f0b51903aadb8e9ae807028b26cb5
Bit-Barron <1bit.baron@gmail.com>
2023-01-01 17:34:05 +0100

    asd

--- web/src/components/elements/Input.tsx
+++ web/src/components/elements/Input.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 
 interface inputProps {
   placeholder: string;
+  onChange: any
+  value: any;
 }
 
-const Input: React.FC<inputProps> = ({ placeholder }) => {
+const Input: React.FC<inputProps> = ({ placeholder, onChange, value }) => {
   return (
     <div className='w-[600px] px-3 mt-10 mx-auto'>
       <input
         className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
         id='grid-password'
         placeholder={placeholder}
+        onChange={(e) => onChange(e.target.value)}
+        value={value}
       />
     </div>
     
--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
-import Input from '../components/elements/input';
+import React, { useState } from 'react';
+import Input from '../components/elements/Input';
 import Button from '../components/elements/Button';
+import Image from 'next/image';
 
-interface LoginProps {}
+interface loginProps {}
 
-const login: React.FC<LoginProps> = ({}) => {
+const login: React.FC<loginProps> = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState<string>('');
+
   return (
     <>
-      <div>
-        <h1 className='text-sm font-semibold text-center mt-52'>Login</h1>
+      <div className='mt-52'>
+        <Image
+          className='mx-auto'
+          src='https://www.letterxpress.de/images/logo.png'
+          alt='a'
+        />
+
+        <h1 className='text-sm font-semibold text-center mt-10'>Login</h1>
         <div className='mb-6 mx-auto'>
-          <Input placeholder='Username' />
-          <Input placeholder='Password' />
+          <Input
+            placeholder='Username'
+            onChange={setUsername}
+            value={username}
+          />
+          <Input
+            placeholder='Password'
+            onChange={setPassword}
+            value={password}
+          />
         </div>
         <Button />
       </div>
