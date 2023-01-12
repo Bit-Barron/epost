0bdd471202e0ef21c752e38a0b37649d161d8353
Bit-Barron <1bit.baron@gmail.com>
2023-01-12 20:09:12 +0100

    asdasd

---
+++ web/src/components/controller/Dashboard/DashboardDesktop.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface DashboardDesktopProps {}
+
+const DashboardDesktop: React.FC<DashboardDesktopProps> = ({}) => {
+  return <div>Test</div>;
+};
+
+export default DashboardDesktop;
---
+++ web/src/components/controller/DashboardContainer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+interface DashboardContainerProps {}
+
+const DashboardContainer: React.FC<DashboardContainerProps> = ({}) => {
+  return <div>test</div>;
+};
+export default DashboardContainer;
