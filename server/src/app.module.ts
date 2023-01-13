b0b4bf0d0aaec30d384cc8107af4aa6bb971f281
Bit-Barron <1bit.baron@gmail.com>
2023-01-13 22:37:10 +0100

    removed

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
@@ -21,7 +19,5 @@ import { Posts } from './posts/posts.entity';
       synchronize: true,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
