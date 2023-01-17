76bf34fe81a4d0dcaae78e4d3ad6fd48d684dc5e
Bit-Barron <1bit.baron@gmail.com>
2023-01-17 15:09:03 +0100

    finished cookie

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -3,5 +3,6 @@ import type { AppProps } from 'next/app';
 import axios from 'axios';
 
 export default function App({ Component, pageProps }: AppProps) {
+  axios.defaults.withCredentials = true;
   return <Component {...pageProps} />;
 }
