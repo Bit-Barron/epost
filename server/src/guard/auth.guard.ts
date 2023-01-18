6f4e676c1992d0b9a482ab02302d55f53d86f3d9
Bit-Barron <1bit.baron@gmail.com>
2023-01-18 13:52:39 +0100

    controller

--- server/src/guard/auth.guard.ts
+++ server/src/guard/auth.guard.ts
@@ -1,9 +1,13 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { CanActivate, ExecutionContext } from '@nestjs/common/interfaces';
+import { Request } from 'express';
 
 export class AuthGuard implements CanActivate {
-  canActivate(context: ExecutionContext): boolean {
-    const request = context.switchToHttp().getRequest();
-    const authToken = request.cookies['jwt'];
-    return !!authToken;
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest<Request>();
+    const auth_token = request.cookies.jwt;
+    if (!auth_token) {
+      throw new Error('Not authenticated');
+    }
+    return true;
   }
 }
