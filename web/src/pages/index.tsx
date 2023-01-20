0b99afa52231ce670bc9f47cfe5c87e402d0097e
Bit-Barron <1bit.baron@gmail.com>
2023-01-20 16:21:23 +0100

    try

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,11 +1,22 @@
 import React, { useEffect } from 'react';
 import DashboardContainer from '../components/controller/DashboardContainer';
 import axios from 'axios';
+import { useRouter } from 'next/router';
 
 const Index: React.FC = ({}) => {
+  const router = useRouter();
   useEffect(() => {
     const getUser = async () => {
-      const user = await axios.post('http://localhost:4000/auth/dashboard');
+      try {
+        const user = await axios.post('http://localhost:4000/auth/dashboard');
+        console.log(user.data);
+      } catch (err) {
+        if (err.response && err.response.status === 401) {
+          router.push('/login');
+        } else {
+          console.log(err);
+        }
+      }
     };
     getUser();
   }, []);
