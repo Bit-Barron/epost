7c102ae9fe52bd0b118303edf19201af1969e210
Bit-Barron <1bit.baron@gmail.com>
2023-01-21 11:04:31 +0100

    finsihed login design

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -41,85 +41,90 @@ const Login: React.FC<loginProps> = () => {
         <span className='self-center font-semibold whitespace-nowrap text-2xl dark:text-white flex'>
           <Image width={80} height={50} src={main} alt='asd' />
         </span>
-        <div className='text-3xl font-bold mt-6 text-secondary'>
-          Postshield
+        <div className='text-3xl font-bold mt-6 text-secondary'>Postshield</div>
+      </div>
+      <hr className='h-px my-8 bg-gray-200' />
+      <div className=''>
+        <div className='flex justify-center border-white'>
+          <button className='bg-blue-500 hover:bg-blue-700 border border-white p-3 w-96 text-white font-bold rounded-full'>
+            WEITER MIT FACEBOOK
+          </button>
         </div>
-      </div>
-      <hr className='h-px my-8 bg-gray-200'/>
-      <div className='flex justify-center border-white'>
-        <button className='bg-blue-500 hover:bg-blue-700 border border-white p-3 w-96 text-white font-bold rounded-full'>
-          WEITER MIT FACEBOOK
-        </button>
-      </div>
-      <div className='flex justify-center mt-3 border-white'>
-        <button className='bg-black  p-3 w-96 text-white border-white  font-bold rounded-full'>
-          WEITER MIT APPLE
-        </button>
-      </div>
-      <div className='flex justify-center mt-3 border-white'>
-        <button className='p-3 w-96 text-white bg-transparent border-2 font-bold rounded-full'>
-          WEITER MIT GOOGLE
-        </button>
-      </div>
-      <div className='inline-flex items-center justify-center w-full'>
-        <hr className='w-96 h-px my-8  border-0bg-gray-700' />
-        <span className='absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white '>
-          or
-        </span>
-      </div>
-      <div>
-        <div className='flex justify-center mr-28 font-bold mb-2'>
-          E-Mail-Adresse oder Benutzername
+        <div className='flex justify-center mt-3 border-white'>
+          <button className='bg-black  p-3 w-96 text-white border-white  font-bold rounded-full'>
+            WEITER MIT APPLE
+          </button>
         </div>
+        <div className='flex justify-center mt-3 border-white'>
+          <button className='p-3 w-96 text-white bg-transparent border-2 font-bold rounded-full'>
+            WEITER MIT GOOGLE
+          </button>
+        </div>
+        <div className='inline-flex items-center justify-center w-full'>
+          <hr className='w-96 h-px my-8  border-0bg-gray-700' />
+          <span className='absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white '>
+            or
+          </span>
+        </div>
+        <div>
+          <div className='flex justify-center mr-28 font-bold mb-2'>
+            E-Mail-Adresse oder Benutzername
+          </div>
 
-        <div className='flex justify-center'>
-          <input
-            type='email'
-            id='helper-text'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            aria-describedby='helper-text-explanation'
-            className='bg-transparent border border-gray-300 text-gray-900 text-sm rounded-sm block w-96 p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white'
-            placeholder='E-Mail-Adresse oder Benutzername'
-          />
-        </div>
-      </div>
-      <div className='mt-10'>
-        <div className='flex mr-[310px] justify-center font-bold mb-2'>
-          Password
-        </div>
-
-        <div className='flex justify-center'>
-          <input
-            type='email'
-            id='helper-text'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            aria-describedby='helper-text-explanation'
-            className='bg-transparent border border-gray-300 text-gray-900 text-sm rounded-sm block w-96 p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white'
-            placeholder='Password'
-          />
-        </div>
-        <div className='flex justify-center mt-5'>
-          <div className='flex mt-5 underline'>Password Vergessen</div>
-          <div className='flex justify-center ml-32'>
-            <button
-              className='bg-secondary p-2 w-32 text-white font-bold rounded-full'
-              onClick={() => submit()}
-            >
-              Anmelden
-            </button>
+          <div className='flex justify-center'>
+            <input
+              type='email'
+              id='helper-text'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              aria-describedby='helper-text-explanation'
+              className='bg-transparent border border-gray-300 text-gray-900 text-sm rounded-sm block w-96 p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white'
+              placeholder='E-Mail-Adresse oder Benutzername'
+            />
           </div>
         </div>
-      </div>
-      <div className='inline-flex items-center justify-center w-full'>
-        <hr className='w-[400px] h-px my-8 border-0bg-gray-700' />
-      </div>
-      <div className='font-bold flex justify-center'>Du hast kein Konto ? </div>
-      <div className='flex justify-center mt-5'>
-        <button className='border-gray-200 p-3 border-2 w-96 bg-transparent text-white font-bold rounded-full' onClick={() => router.push("/register")}>
-          Bei Postshield Regestriere
-        </button>
+        <div className='mt-10'>
+          <div className='flex mr-[310px] justify-center font-bold mb-2'>
+            Password
+          </div>
+
+          <div className='flex justify-center'>
+            <input
+              type='email'
+              id='helper-text'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              aria-describedby='helper-text-explanation'
+              className='bg-transparent border border-gray-300 text-gray-900 text-sm rounded-sm block w-96 p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white'
+              placeholder='Password'
+            />
+          </div>
+          <div className='flex justify-center mt-5'>
+            <div className='flex mt-5 underline'>Password Vergessen</div>
+            <div className='flex justify-center ml-32'>
+              <button
+                className='bg-secondary p-2 w-32 text-white font-bold rounded-full'
+                onClick={() => submit()}
+              >
+                Anmelden
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='inline-flex items-center justify-center w-full'>
+          <hr className='w-[400px] h-px my-8 border-0bg-gray-700' />
+        </div>
+        <div className='font-bold flex justify-center'>
+          Du hast kein Konto ?{' '}
+        </div>
+        <div className='flex justify-center mt-5'>
+          <button
+            className='border-gray-200 p-3 border-2 w-96 bg-transparent text-white font-bold rounded-full'
+            onClick={() => router.push('/register')}
+          >
+            Bei Postshield Regestriere
+          </button>
+        </div>
       </div>
     </>
   );
