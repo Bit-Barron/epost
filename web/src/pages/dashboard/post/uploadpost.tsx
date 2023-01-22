e0cdcd9328f2697c06654c4bb165a3503b652d0d
Bit-Barron <1bit.baron@gmail.com>
2023-01-22 18:38:30 +0100

    upload.tsx

--- web/src/pages/dashboard/post/uploadpost.tsx
+++ web/src/pages/dashboard/post/uploadpost.tsx
@@ -6,10 +6,10 @@ interface uploadpostProps {}
 const uploadpost: React.FC<uploadpostProps> = ({}) => {
   return (
     <DashboardContainer>
-      <div className='text-center font-bold text-2xl mt-10'>
+      <div className='text-center font-bold text-2xl  mt-40'>
         Mit LXP GO clever Rechnungen, Mahnungen und Mailings online versenden
       </div>
-      <div className='w-1/2 mt-10 mx-auto '>
+      <div className='w-1/2 mx-auto mt-10'>
         <label className='flex justify-center w-full p-10 px-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none'>
           <span className='flex items-center space-x-2'>
             <svg
@@ -34,6 +34,23 @@ const uploadpost: React.FC<uploadpostProps> = ({}) => {
           </span>
           <input type='file' name='file_upload' className='hidden' />
         </label>
+      </div>
+      <div className='bg-gray-200 mx-auto mt-10 p-4 w-1/2'>
+        <div className='font-bold text-2xl text-gray-500'>
+          Wusten sie schon?
+        </div>
+        <div className='mt-5 text-gray-500'>
+          Schreiben – klicken – verschicken! Briefe können Sie jetzt auch direkt
+          im Browser in einer vorgegebenen Briefvorlage schreiben <br /> und
+          verschicken. Ideal auch als Deckblatt oder Kurznotiz für PDF-Anhänge
+          zu nutzen.{' '}
+          <span className='font-bold hover:underline'>Jetzt ausprobieren</span>
+        </div>
+      </div>
+      <div className='flex justify-center mt-5'>
+        <button className='bg-secondary text-white font-bold py-2 px-4 rounded'>
+          BRIEF ZUR POSTBOX HINZUFÜGEN
+        </button>
       </div>
     </DashboardContainer>
   );
