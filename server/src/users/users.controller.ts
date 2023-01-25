69eb1a027a7d3eb5a8d9715cf7ff540b053c69fd
Bit-Barron <1bit.baron@gmail.com>
2023-01-25 22:31:46 +0100

    auth.guard

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Post, Body, Res, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Res,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { CreateUserDto } from '../users/dtos/create-user.dto';
 import { AuthService } from './auth.service';
 import { Response } from 'express';
