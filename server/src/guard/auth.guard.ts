eb7498ef2ccb5929bfd0fb7df1eece0c602bf3c2
Bit-Barron <1bit.baron@gmail.com>
2023-01-26 15:45:23 +0100

    auth.guard

--- server/src/guard/auth.guard.ts
+++ server/src/guard/auth.guard.ts
@@ -1,9 +1,19 @@
 import { CanActivate, ExecutionContext } from '@nestjs/common/interfaces';
 import { Request } from 'express';
-
+import * as jwt from 'jsonwebtoken';
 export class AuthGuard implements CanActivate {
   canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest<Request>();
-    return request.cookies['jwt'];
+    const token = request.cookies['jwt'];
+    if (!token) {
+      return false;
+    }
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      request.user = decoded;
+      return true;
+    } catch (err) {
+      return false;
+    }
   }
 }
