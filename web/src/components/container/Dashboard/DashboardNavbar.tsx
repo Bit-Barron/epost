512d2ff5759e1e8c757acc60436986c716c075f1
Bit-Barron <1bit.baron@gmail.com>
2023-01-27 18:46:18 +0100

    navbar

--- web/src/components/container/Dashboard/DashboardNavbar.tsx
+++ web/src/components/container/Dashboard/DashboardNavbar.tsx
@@ -4,12 +4,16 @@ import { AiOutlineArrowDown } from 'react-icons/ai';
 
 const DashboardNavbar = () => {
   const [user, setUser] = useState<any[]>([]);
+  const [test, setTest] = useState('');
   useEffect(() => {
     const getUser = async () => {
       const response = await axios.get('http://localhost:4000/auth/user');
+      console.log(response.data.email);
+      setUser(response.data);
     };
     getUser();
   }, []);
+  console.log(test)
 
   return (
     <div className='border-b border-gray-200 px-4 py-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8'>
@@ -31,9 +35,6 @@ const DashboardNavbar = () => {
           type='button'
           className='order-1 ml-3 inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-0 sm:ml-0'
         >
-          {user.map((item) => {
-            return item.username;
-          })}
           <AiOutlineArrowDown className='ml-1' />
         </button>
       </div>
