05d32ab74d32084d7fc1af70858945f3955dee8f
Bit-Barron <1bit.baron@gmail.com>
2023-01-27 21:21:32 +0100

    clean up

--- web/src/pages/dashboard/postshield/price.tsx
+++ web/src/pages/dashboard/postshield/price.tsx
@@ -1,12 +1,14 @@
 import { Popover, Transition } from '@headlessui/react';
 import {
-    BookmarkAltIcon,
-    CalendarIcon,
-    ChartBarIcon, CursorClickIcon, RefreshIcon,
-    ShieldCheckIcon,
-    SupportIcon,
-    ViewGridIcon,
-    XIcon
+  BookmarkAltIcon,
+  CalendarIcon,
+  ChartBarIcon,
+  CursorClickIcon,
+  RefreshIcon,
+  ShieldCheckIcon,
+  SupportIcon,
+  ViewGridIcon,
+  XIcon,
 } from '@heroicons/react/outline';
 import { CheckIcon as CheckIconSolid } from '@heroicons/react/solid';
 import Image from 'next/image';
@@ -163,7 +165,7 @@ const faqs = [
   },
 ];
 
-export default function Example() {
+function Price() {
   return (
     <div className='bg-white'>
       <Popover className='relative bg-white'>
@@ -267,7 +269,7 @@ export default function Example() {
                     Sign up
                   </a>
                   <p className='text-center text-base font-medium text-gray-500'>
-                    Existing customer?{' '}
+                    Existing customer?
                     <a
                       href='#'
                       className='text-purple-600 hover:text-purple-500'
@@ -346,35 +348,9 @@ export default function Example() {
         <Partnership />
       </div>
 
-      <div className='max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-20 lg:px-8'>
-        <div className='lg:grid lg:grid-cols-3 lg:gap-8'>
-          <div className='space-y-4'>
-            <h2 className='text-3xl font-extrabold text-gray-900'>
-              Frequently asked questions
-            </h2>
-            <p className='text-lg text-gray-500'>
-              Can’t find the answer you’re looking for? Reach out to our
-              <a href='#' className='font-medium text-secondary'>
-                customer support
-              </a>
-              team.
-            </p>
-          </div>
-          <div className='mt-12 lg:mt-0 lg:col-span-2'>
-            <dl className='space-y-12'>
-              {faqs.map((faq) => (
-                <div key={faq.id}>
-                  <dt className='text-lg leading-6 font-medium text-gray-900'>
-                    {faq.question}
-                  </dt>
-                  <dd className='mt-2 text-base text-gray-500'>{faq.answer}</dd>
-                </div>
-              ))}
-            </dl>
-          </div>
-        </div>
-      </div>
       <Impresum />
     </div>
   );
 }
+
+export default Price;
