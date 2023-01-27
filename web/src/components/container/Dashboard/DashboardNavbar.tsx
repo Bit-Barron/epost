7b49228d23c9dd24951de8bc96b77af79ff2043e
Bit-Barron <1bit.baron@gmail.com>
2023-01-27 19:02:16 +0100

    navbar.tsx

--- web/src/components/container/Dashboard/DashboardNavbar.tsx
+++ web/src/components/container/Dashboard/DashboardNavbar.tsx
@@ -4,16 +4,20 @@ import { AiOutlineArrowDown } from 'react-icons/ai';
 
 const DashboardNavbar = () => {
   const [user, setUser] = useState<any[]>([]);
-  const [test, setTest] = useState('');
   useEffect(() => {
     const getUser = async () => {
-      const response = await axios.get('http://localhost:4000/auth/user');
-      console.log(response.data.email);
-      setUser(response.data);
+      try {
+        const response = await axios.get<any>(
+          'http://localhost:4000/auth/user'
+        );
+        console.log(response.data);
+        setUser(response.data);
+      } catch (err) {
+        console.log(err);
+      }
     };
     getUser();
   }, []);
-  console.log(test)
 
   return (
     <div className='border-b border-gray-200 px-4 py-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8'>
@@ -35,6 +39,9 @@ const DashboardNavbar = () => {
           type='button'
           className='order-1 ml-3 inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-0 sm:ml-0'
         >
+          {user.map((item) => (
+            <div>{item.email}</div>
+          ))}
           <AiOutlineArrowDown className='ml-1' />
         </button>
       </div>
--- web/src/types/index.d.ts
+++ web/src/types/index.d.ts
@@ -6,10 +6,16 @@ export type DashboardTab = {
 
 export type DashboardTabType = 'Dashboard' | 'Order overview' | 'Recent';
 
-export type AdminTabType = "Products" | "Categories" | "Attributes" | "Users";
+export type AdminTabType = 'Products' | 'Categories' | 'Attributes' | 'Users';
 
 export type AdminTab = {
   name: AdminTabType;
   Icon: IconType;
   current: boolean;
 };
+
+export interface Root {
+  email: string;
+  sub: number;
+  iat: number;
+}
