17f858fcced61abca87ae9415e8592df911db0c9
Bit-Barron <1bit.baron@gmail.com>
2023-01-31 00:29:07 +0100

    user.tsx

---
+++ web/src/pages/user/user.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AdminContainer from '../../components/container/AdminContainer';
+
+interface userProps {}
+
+const user: React.FC<userProps> = ({}) => {
+  return <AdminContainer></AdminContainer>
+};
+
+export default user;
