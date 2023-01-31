9c4f4b7bcd4070e8de06293ca16b75f055ce20c7
Bit-Barron <1bit.baron@gmail.com>
2023-01-31 22:31:00 +0100

    controller

--- server/src/letter/letter.service.ts
+++ server/src/letter/letter.service.ts
@@ -9,23 +9,4 @@ export class LetterService {
     @InjectRepository(Letter)
     private letterRepo: Repository<Letter>,
   ) {}
-<<<<<<< HEAD
-  async create(letter: CreateLetterDto): Promise<Letter> {
-    const newLetter = this.letterRepo.create({
-      content: letter.content,
-      title: letter.title,
-      user: { id: letter.userId },
-    });
-    return await this.letterRepo.save(newLetter);
-  }
-
-  async findOne(id: number) {
-    return await this.letterRepo.findOneBy({ id });
-  }
-
-  async findAll() {
-    return await this.letterRepo.find({ where: { userId } });
-  }
-=======
->>>>>>> 0debef17e27da9f40e2e0876912dddaa69a44a1c
 }
