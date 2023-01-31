0debef17e27da9f40e2e0876912dddaa69a44a1c
Don-Cryptus <don.cryptus@gmail.com>
2023-01-31 15:17:33 +0100

    fix

--- server/src/letter/letter.controller.ts
+++ server/src/letter/letter.controller.ts
@@ -1,13 +1,20 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Param, Req, UseGuards } from '@nestjs/common/decorators';
+import { InjectRepository } from '@nestjs/typeorm';
 import { FastifyRequest } from 'fastify';
 import { AuthGuard } from 'src/app_modules/guard/auth.guard';
+import { Repository } from 'typeorm';
 import { CreateLetterDto } from './dtos/create-letter.dto';
+import { Letter } from './letter.entity';
 import { LetterService } from './letter.service';
 
 @Controller('letter')
 export class LetterController {
-  constructor(private letterService: LetterService) {}
+  constructor(
+    private letterService: LetterService,
+    @InjectRepository(Letter)
+    private letterRepo: Repository<Letter>,
+  ) {}
 
   @Post('/create')
   @UseGuards(AuthGuard)
@@ -17,17 +24,27 @@ export class LetterController {
   ) {
     createLetterDto.userId = req.user.sub;
 
-    return await this.letterService.create(createLetterDto);
+    const newLetter = this.letterRepo.create({
+      content: createLetterDto.content,
+      title: createLetterDto.title,
+      user: { id: createLetterDto.userId },
+    });
+    return await this.letterRepo.save(newLetter);
   }
 
   @UseGuards(AuthGuard)
-  @Get('/all')
-  async findAllUsers() {
-    return await this.letterService.findAll();
+  @Get('/all-user')
+  async findAllUsers(@Req() req: FastifyRequest) {
+    return await this.letterRepo.find({
+      where: { user: { id: req.user.sub } },
+    });
   }
 
+  @UseGuards(AuthGuard)
   @Get('/:id')
-  async findOneUser(@Param('id') id: number) {
-    return await this.letterService.findOne(id);
+  async findOneUser(@Param('id') id: number, @Req() req: FastifyRequest) {
+    return await this.letterRepo.findOne({
+      where: { id, user: { id: req.user.sub } },
+    });
   }
 }
--- server/src/letter/letter.service.ts
+++ server/src/letter/letter.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { CreateLetterDto } from './dtos/create-letter.dto';
 import { Letter } from './letter.entity';
 
 @Injectable()
@@ -10,20 +9,4 @@ export class LetterService {
     @InjectRepository(Letter)
     private letterRepo: Repository<Letter>,
   ) {}
-  async create(letter: CreateLetterDto): Promise<Letter> {
-    const newLetter = this.letterRepo.create({
-      content: letter.content,
-      title: letter.title,
-      user: { id: letter.userId },
-    });
-    return await this.letterRepo.save(newLetter);
-  }
-
-  async findOne(id: number) {
-    return await this.letterRepo.findOneBy({ id });
-  }
-
-  async findAll() {
-    return await this.letterRepo.find();
-  }
 }
