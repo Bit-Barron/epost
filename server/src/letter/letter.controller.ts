39959dad2ad43b53553bd602223155b44e41dc99
Bit-Barron <1bit.baron@gmail.com>
2023-02-02 00:34:21 +0100

    letter.controller

--- server/src/letter/letter.controller.ts
+++ server/src/letter/letter.controller.ts
@@ -61,4 +61,13 @@ export class LetterController {
     } catch (err) {}
     return true;
   }
+  @UseGuards(AuthGuard)
+  @Delete('/all-user')
+  async deleteAll(@Param('id') id: number, @Req() req: FastifyRequest) {
+    console.log(req.user);
+    try {
+      return await this.letterRepo.delete({ id, user: { id: req.user.sub } });
+    } catch (err) {}
+    return true;
+  }
 }
