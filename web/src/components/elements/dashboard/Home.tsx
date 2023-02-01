c5e2f68abbb593ff463f6344be1249de839428ef
Bit-Barron <1bit.baron@gmail.com>
2023-02-01 01:14:33 +0100

    Home.tsx

--- web/src/components/elements/dashboard/Home.tsx
+++ web/src/components/elements/dashboard/Home.tsx
@@ -1,4 +1,3 @@
-import { Fragment } from 'react';
 import { Menu, Popover, Transition } from '@headlessui/react';
 import {
   AcademicCapIcon,
@@ -6,12 +5,12 @@ import {
   BellIcon,
   CashIcon,
   ClockIcon,
-  MenuIcon,
   ReceiptRefundIcon,
   UsersIcon,
   XIcon,
 } from '@heroicons/react/outline';
-import { SearchIcon } from '@heroicons/react/solid';
+import { Fragment } from 'react';
+import Impresum from '../pricepage/price/impresum/Impresum';
 
 const user = {
   name: 'Chelsea Hagon',
@@ -32,11 +31,7 @@ const userNavigation = [
   { name: 'Settings', href: '#' },
   { name: 'Sign out', href: '#' },
 ];
-const stats = [
-  { label: 'Vacation days left', value: 12 },
-  { label: 'Sick days left', value: 4 },
-  { label: 'Personal days left', value: 2 },
-];
+
 const actions = [
   {
     icon: ClockIcon,
@@ -147,48 +142,19 @@ export default function Home() {
           as='header'
           className='pb-24 bg-gradient-to-r from-sky-800 to-cyan-600'
         >
-          {({ open }) => (
+          {() => (
             <>
               <div className='max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8'>
                 <div className='relative flex flex-wrap items-center justify-center lg:justify-between'>
-                  {/* Logo */}
-                  <div className='absolute left-0 py-5 flex-shrink-0 lg:static'>
-                    <a href='#'>
-                      <span className='sr-only'>Workflow</span>
-                      <svg
-                        className='h-8 w-auto'
-                        fill='none'
-                        viewBox='0 0 35 32'
-                        xmlns='http://www.w3.org/2000/svg'
-                      >
-                        <path
-                          fill='#A5F3FC'
-                          d='M15.258 26.865a4.043 4.043 0 01-1.133 2.917A4.006 4.006 0 0111.253 31a3.992 3.992 0 01-2.872-1.218 4.028 4.028 0 01-1.133-2.917c.009-.698.2-1.382.557-1.981.356-.6.863-1.094 1.47-1.433-.024.109.09-.055 0 0l1.86-1.652a8.495 8.495 0 002.304-5.793c0-2.926-1.711-5.901-4.17-7.457.094.055-.036-.094 0 0A3.952 3.952 0 017.8 7.116a3.975 3.975 0 01-.557-1.98 4.042 4.042 0 011.133-2.918A4.006 4.006 0 0111.247 1a3.99 3.99 0 012.872 1.218 4.025 4.025 0 011.133 2.917 8.521 8.521 0 002.347 5.832l.817.8c.326.285.668.551 1.024.798.621.33 1.142.826 1.504 1.431a3.902 3.902 0 01-1.504 5.442c.033-.067-.063.036 0 0a8.968 8.968 0 00-3.024 3.183 9.016 9.016 0 00-1.158 4.244zM19.741 5.123c0 .796.235 1.575.676 2.237a4.01 4.01 0 001.798 1.482 3.99 3.99 0 004.366-.873 4.042 4.042 0 00.869-4.386 4.02 4.02 0 00-1.476-1.806 3.994 3.994 0 00-5.058.501 4.038 4.038 0 00-1.175 2.845zM23.748 22.84c-.792 0-1.567.236-2.226.678a4.021 4.021 0 00-1.476 1.806 4.042 4.042 0 00.869 4.387 3.99 3.99 0 004.366.873A4.01 4.01 0 0027.08 29.1a4.039 4.039 0 00-.5-5.082 4 4 0 00-2.832-1.18zM34 15.994c0-.796-.235-1.574-.675-2.236a4.01 4.01 0 00-1.798-1.483 3.99 3.99 0 00-4.367.873 4.042 4.042 0 00-.869 4.387 4.02 4.02 0 001.476 1.806 3.993 3.993 0 002.226.678 4.003 4.003 0 002.832-1.18A4.04 4.04 0 0034 15.993z M5.007 11.969c-.793 0-1.567.236-2.226.678a4.021 4.021 0 00-1.476 1.807 4.042 4.042 0 00.869 4.386 4.001 4.001 0 004.366.873 4.011 4.011 0 001.798-1.483 4.038 4.038 0 00-.5-5.08 4.004 4.004 0 00-2.831-1.181z'
-                        />
-                      </svg>
-                    </a>
-                  </div>
-
                   <div className='hidden lg:ml-4 lg:flex lg:items-center lg:py-5 lg:pr-0.5'>
                     <button
                       type='button'
                       className='flex-shrink-0 p-1 text-cyan-200 rounded-full hover:text-white hover:bg-white hover:bg-opacity-10 focus:outline-none focus:ring-2 focus:ring-white'
                     >
                       <span className='sr-only'>View notifications</span>
-                      <BellIcon className='h-6 w-6' aria-hidden='true' />
                     </button>
 
                     <Menu as='div' className='ml-4 relative flex-shrink-0'>
-                      <div>
-                        <Menu.Button className='bg-white rounded-full flex text-sm ring-2 ring-white ring-opacity-20 focus:outline-none focus:ring-opacity-100'>
-                          <span className='sr-only'>Open user menu</span>
-                          <img
-                            className='h-8 w-8 rounded-full'
-                            src={user.imageUrl}
-                            alt=''
-                          />
-                        </Menu.Button>
-                      </div>
                       <Transition
                         as={Fragment}
                         leave='transition ease-in duration-75'
@@ -219,17 +185,6 @@ export default function Home() {
                   {/* Menu button */}
                   <div className='absolute right-0 flex-shrink-0 lg:hidden'>
                     {/* Mobile menu button */}
-                    <Popover.Button className='bg-transparent p-2 rounded-md inline-flex items-center justify-center text-cyan-200 hover:text-white hover:bg-white hover:bg-opacity-10 focus:outline-none focus:ring-2 focus:ring-white'>
-                      <span className='sr-only'>Open main menu</span>
-                      {open ? (
-                        <XIcon className='block h-6 w-6' aria-hidden='true' />
-                      ) : (
-                        <MenuIcon
-                          className='block h-6 w-6'
-                          aria-hidden='true'
-                        />
-                      )}
-                    </Popover.Button>
                   </div>
                 </div>
               </div>
@@ -356,13 +311,6 @@ export default function Home() {
                     <div className='bg-white p-6'>
                       <div className='sm:flex sm:items-center sm:justify-between'>
                         <div className='sm:flex sm:space-x-5'>
-                          <div className='flex-shrink-0'>
-                            <img
-                              className='mx-auto h-20 w-20 rounded-full'
-                              src={user.imageUrl}
-                              alt=''
-                            />
-                          </div>
                           <div className='mt-4 text-center sm:mt-0 sm:pt-1 sm:text-left'>
                             <p className='text-sm font-medium text-gray-600'>
                               Welcome back,
@@ -384,17 +332,6 @@ export default function Home() {
                           </a>
                         </div>
                       </div>
-                    </div>
-                    <div className='border-t border-gray-200 bg-gray-50 grid grid-cols-1 divide-y divide-gray-200 sm:grid-cols-3 sm:divide-y-0 sm:divide-x'>
-                      {stats.map((stat) => (
-                        <div
-                          key={stat.label}
-                          className='px-6 py-5 text-sm font-medium text-center'
-                        >
-                          <span className='text-gray-900'>{stat.value}</span>{' '}
-                          <span className='text-gray-600'>{stat.label}</span>
-                        </div>
-                      ))}
                     </div>
                   </div>
                 </section>
@@ -456,19 +393,6 @@ export default function Home() {
                             blanditiis et quo et molestiae.
                           </p>
                         </div>
-                        <span
-                          className='pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400'
-                          aria-hidden='true'
-                        >
-                          <svg
-                            className='h-6 w-6'
-                            xmlns='http://www.w3.org/2000/svg'
-                            fill='currentColor'
-                            viewBox='0 0 24 24'
-                          >
-                            <path d='M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z' />
-                          </svg>
-                        </span>
                       </div>
                     ))}
                   </div>
@@ -527,77 +451,12 @@ export default function Home() {
                     </div>
                   </div>
                 </section>
-
-                {/* Recent Hires */}
-                <section aria-labelledby='recent-hires-title'>
-                  <div className='rounded-lg bg-white overflow-hidden shadow'>
-                    <div className='p-6'>
-                      <h2
-                        className='text-base font-medium text-gray-900'
-                        id='recent-hires-title'
-                      >
-                        Recent Hires
-                      </h2>
-                      <div className='flow-root mt-6'>
-                        <ul
-                          role='list'
-                          className='-my-5 divide-y divide-gray-200'
-                        >
-                          {recentHires.map((person) => (
-                            <li key={person.handle} className='py-4'>
-                              <div className='flex items-center space-x-4'>
-                                <div className='flex-shrink-0'>
-                                  <img
-                                    className='h-8 w-8 rounded-full'
-                                    src={person.imageUrl}
-                                    alt=''
-                                  />
-                                </div>
-                                <div className='flex-1 min-w-0'>
-                                  <p className='text-sm font-medium text-gray-900 truncate'>
-                                    {person.name}
-                                  </p>
-                                  <p className='text-sm text-gray-500 truncate'>
-                                    {'@' + person.handle}
-                                  </p>
-                                </div>
-                                <div>
-                                  <a
-                                    href={person.href}
-                                    className='inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50'
-                                  >
-                                    View
-                                  </a>
-                                </div>
-                              </div>
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                      <div className='mt-6'>
-                        <a
-                          href='#'
-                          className='w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50'
-                        >
-                          View all
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </section>
               </div>
             </div>
           </div>
         </main>
         <footer>
-          <div className='max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl'>
-            <div className='border-t border-gray-200 py-8 text-sm text-gray-500 text-center sm:text-left'>
-              <span className='block sm:inline'>
-                &copy; 2021 Tailwind Labs Inc.
-              </span>{' '}
-              <span className='block sm:inline'>All rights reserved.</span>
-            </div>
-          </div>
+          <Impresum />
         </footer>
       </div>
     </>
