8f9c116ff84bca33897e1df3851bbf4a37f34d9d
Bit-Barron <1bit.baron@gmail.com>
2023-02-03 14:11:52 +0100

    personalinformations

--- web/src/pages/dashboard/settings/personalinformations.tsx
+++ web/src/pages/dashboard/settings/personalinformations.tsx
@@ -12,17 +12,18 @@ const personalinformations: React.FC<settingsProps> = ({}) => {
           <div className='border-t border-gray-200'>
             <dl>
               <div className='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
-                <dt className='text-sm font-medium text-gray-500'>Full name</dt>
+                <dt className='text-sm font-medium text-gray-500'>Firma</dt>
                 <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>
                   Margot Foster
                 </dd>
               </div>
               <div className='bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
-                <dt className='text-sm font-medium text-gray-500'>
-                  Application for
-                </dt>
-                <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>
-                  Backend Developer
+                <dt className='text-sm font-medium text-gray-500'>Anrede</dt>
+                <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0 flex flex-col'>
+                  <span className='align-self-center'>Backend Developer</span>
+                  <div className='flex justify-end align-self-center justify-self-end'>
+                    edit
+                  </div>
                 </dd>
               </div>
               <div className='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
@@ -34,15 +35,13 @@ const personalinformations: React.FC<settingsProps> = ({}) => {
                 </dd>
               </div>
               <div className='bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
-                <dt className='text-sm font-medium text-gray-500'>
-                  Salary expectation
-                </dt>
+                <dt className='text-sm font-medium text-gray-500'>vorname</dt>
                 <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>
                   $120,000
                 </dd>
               </div>
               <div className='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
-                <dt className='text-sm font-medium text-gray-500'>About</dt>
+                <dt className='text-sm font-medium text-gray-500'>Nachname</dt>
                 <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>
                   Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
                   incididunt cillum culpa consequat. Excepteur qui ipsum aliquip
@@ -52,33 +51,47 @@ const personalinformations: React.FC<settingsProps> = ({}) => {
                 </dd>
               </div>
               <div className='bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
-                <dt className='text-sm font-medium text-gray-500'>
-                  Attachments
-                </dt>
+                <dt className='text-sm font-medium text-gray-500'>straße</dt>
                 <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>
                   <ul
                     role='list'
                     className='divide-y divide-gray-200 rounded-md border border-gray-200'
-                  >
-                    <li className='flex items-center justify-between py-3 pl-3 pr-4 text-sm'>
-                      <div className='flex w-0 flex-1 items-center'>
-                        <span className='ml-2 w-0 flex-1 truncate'>
-                          coverletter_back_end_developer.pdf
-                        </span>
-                      </div>
-                      <div className='ml-4 flex-shrink-0'>
-                        <a
-                          href='#'
-                          className='font-medium text-indigo-600 hover:text-indigo-500'
-                        >
-                          Download
-                        </a>
-                      </div>
-                    </li>
-                  </ul>
+                  ></ul>
+                </dd>
+              </div>
+              <div className='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                <dt className='text-sm font-medium text-gray-500'>PLZ</dt>
+                <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>
+                  margotfoster@example.com
+                </dd>
+              </div>
+              <div className='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                <dt className='text-sm font-medium text-gray-500'>Ort</dt>
+                <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>
+                  margotfoster@example.com
+                </dd>
+              </div>
+              <div className='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                <dt className='text-sm font-medium text-gray-500'>LAND</dt>
+                <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>
+                  margotfoster@example.com
+                </dd>
+              </div>
+              <div className='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                <dt className='text-sm font-medium text-gray-500'>Telefon</dt>
+                <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>
+                  Telefon
                 </dd>
               </div>
             </dl>
+          </div>
+          <div className='mt-5 flex justify-end'>
+            <button
+              type='button'
+              className='inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-secondary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+            >
+              Änderungen speichern
+            </button>
           </div>
         </div>
       </SettingsContainer>
