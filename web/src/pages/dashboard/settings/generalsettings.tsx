8f6006ead7d7334b824acad69cbb0aca1a5a30f6
Bit-Barron <1bit.baron@gmail.com>
2023-02-05 19:13:29 +0100

    generalsettings.tsx

--- web/src/pages/dashboard/settings/generalsettings.tsx
+++ web/src/pages/dashboard/settings/generalsettings.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { DashboardContainer } from '../../../components/container/DashboardContainer';
 import { SettingsContainer } from '../../../components/container/SettingsContainer';
 import AuthButton from '../../../components/elements/auth/AuthButton';
+import AuthInput from '../../../components/elements/auth/AuthInput';
 
 interface settingsProps {}
 
@@ -10,7 +11,10 @@ const generalsettings: React.FC<settingsProps> = ({}) => {
     <DashboardContainer>
       <SettingsContainer>
         <div className='bg-[#181a1b] p-5'>
-            <div className='font-bold text-xl'>Rechnungen</div>
+          <div className='font-bold text-xl'>Rechnungen</div>
+          <div className='flex justify-start'>
+            <AuthInput type={'text'} label={'Einzelverbindungsnachweis bei Rechnungen immer mit anfügen'} />
+          </div>
         </div>
       </SettingsContainer>
     </DashboardContainer>
