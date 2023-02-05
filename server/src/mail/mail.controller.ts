7bb86f9efa97b661e1597ed5470dc91eb5c0f1d2
Bit-Barron <1bit.baron@gmail.com>
2023-02-05 14:26:34 +0100

    nodemailer

--- server/src/mail/mail.controller.ts
+++ server/src/mail/mail.controller.ts
@@ -1,9 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import nodemailer from 'nodemailer';
 import { CreateMailDto } from './dtos/create-mail.dto';
 import { MailService } from './mail.service';
 
 @Controller('mail')
 export class MailController {
+  private readonly transporter: nodemailer.Transporter;
   constructor(private readonly mailService: MailService) {}
 
   @Post('/get-mail')
