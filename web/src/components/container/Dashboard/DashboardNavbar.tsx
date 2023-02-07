f34ce097beca766249aa686ad69e639f8b4bd03f
Bit-Barron <1bit.baron@gmail.com>
2023-02-07 21:27:38 +0100

    dashboardnarvbar

--- web/src/components/container/Dashboard/DashboardNavbar.tsx
+++ web/src/components/container/Dashboard/DashboardNavbar.tsx
@@ -21,7 +21,7 @@ const DashboardNavbar = () => {
 
   return (
     <div className='border-b border-gray-200 px-4 py-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8'>
-      <div className=''>
+      <div>
         <h1 className='text-lg font-medium leading-6 text-white sm:truncate'>
           Dashboard
         </h1>
@@ -37,7 +37,7 @@ const DashboardNavbar = () => {
           </Link>
         </div>
         <Menu as='div'>
-          <Menu.Button className='ounded-md bg-[#25282A] py-2 px-4 text-white'>
+          <Menu.Button className='rounded-md bg-[#25282A] py-2 px-4 text-white'>
             {user}
           </Menu.Button>
           <Profile />
