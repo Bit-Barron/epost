c59575d29358e11082afda8ab0036e643d0fd6ef
Bit-Barron <1bit.baron@gmail.com>
2023-02-08 14:52:47 +0100

    poststable

--- web/src/components/elements/postboxPage/PostsTable.tsx
+++ web/src/components/elements/postboxPage/PostsTable.tsx
@@ -30,12 +30,12 @@ function PostsTable() {
   };
 
   const deleteallposts = async () => {
-    const response = await axios.delete('/letter/all-letter');
+    await axios.delete('/letter/all-letter');
     getPosts();
   };
 
   async function deleteposts(id: number) {
-    const response = await axios.delete(`/letter/${id}`);
+    await axios.delete(`/letter/${id}`);
     getPosts();
   }
 
@@ -46,27 +46,27 @@ function PostsTable() {
   let date = dayjs().format('DD.MM.YYYY');
 
   return (
-    <div className='px-4 sm:px-6 lg:px-8'>
-      <div className='sm:flex sm:items-center'>
-        <div className='sm:flex-auto'>
-          <h1 className='text-xl font-semibold text-gray-900'>Users</h1>
-          <p className='mt-2 text-sm text-gray-700'>
+    <div className="px-4 sm:px-6 lg:px-8">
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-xl font-semibold text-gray-900">Users</h1>
+          <p className="mt-2 text-sm text-gray-700">
             A list of all the users in your account including their name, title,
             email and role.
           </p>
         </div>
-        <div className='mt-4 sm:mt-0 sm:ml-16 sm:flex-none flex'>
+        <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none flex">
           <button
             onClick={() => deleteallposts()}
-            type='button'
-            className='inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto'
+            type="button"
+            className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
           >
             POSTBOX LEEREN
           </button>
-          <div className='ml-10'>
+          <div className="ml-10">
             <button
-              type='button'
-              className='inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto'
+              type="button"
+              className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
             >
               SENDUNGEN ZUSAMMENFÜHREN
             </button>
@@ -74,106 +74,106 @@ function PostsTable() {
         </div>
       </div>
 
-      <div className='mt-8 flex flex-col'>
-        <div className='-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8'>
-          <div className='inline-block min-w-full py-2 align-middle md:px-6 lg:px-8'>
-            <div className='overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg'>
-              <table className='min-w-full divide-y divide-gray-300'>
-                <thead className='bg-gray-50'>
+      <div className="mt-8 flex flex-col">
+        <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-300">
+                <thead className="bg-gray-50">
                   <tr>
                     <th
-                      scope='col'
-                      className='py-3 pl-4 pr-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500 sm:pl-6'
+                      scope="col"
+                      className="py-3 pl-4 pr-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500 sm:pl-6"
                     >
                       Dokument / Eigenschaften
                     </th>
                     <th
-                      scope='col'
-                      className='px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500'
+                      scope="col"
+                      className="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
                     >
                       Briefe
                     </th>
                     <th
-                      scope='col'
-                      className='px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500'
+                      scope="col"
+                      className="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
                     >
                       Seiten
                     </th>
                     <th
-                      scope='col'
-                      className='px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500'
+                      scope="col"
+                      className="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
                     >
                       Umschlag
                     </th>
 
                     <th
-                      scope='col'
-                      className='px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500'
+                      scope="col"
+                      className="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
                     >
                       Hinzugefügt
                     </th>
                     <th
-                      scope='col'
-                      className='px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500'
+                      scope="col"
+                      className="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
                     >
                       Preis
                     </th>
                     <th
-                      scope='col'
-                      className='px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500'
+                      scope="col"
+                      className="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
                     >
                       PDF
                     </th>
-                    <th scope='col' className='relative py-3 pl-3 pr-4 sm:pr-6'>
-                      <span className='sr-only'>Edit</span>
+                    <th scope="col" className="relative py-3 pl-3 pr-4 sm:pr-6">
+                      <span className="sr-only">Edit</span>
                     </th>
                   </tr>
                 </thead>
-                <tbody className='divide-y divide-gray-200 bg-white'>
+                <tbody className="divide-y divide-gray-200 bg-white">
                   {data.map((letter, index) => (
-                    <tr key={index} className='hover:bg-gray-200'>
-                      <td className='whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6'>
+                    <tr key={index} className="hover:bg-gray-200">
+                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
                         {letter.documents}
-                        <div className='text-xs text-gray-500 mt-2'>
+                        <div className="text-xs text-gray-500 mt-2">
                           {letter.side}, {letter.color}
                         </div>
                       </td>
-                      <td className='whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6'>
+                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
                         {letter.posts}
                       </td>
-                      <td className='whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6'>
+                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
                         {letter.pages}
                       </td>
-                      <td className='whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6'>
+                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
                         {letter.envelope}
                       </td>
-                    
-                      <td className='whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6'>
+
+                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
                         {date}
                       </td>
-                      <td className='whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6'>
+                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
                         {letter.price}€
                       </td>
-                      <td className='whitespace-nowrap py-2 pl-2 pr-1 text-sm font-medium text-gray-900 sm:pl-6 '>
-                        <BsFillFileEarmarkPdfFill className='text-xl text-pink-500' />
+                      <td className="whitespace-nowrap py-2 pl-2 pr-1 text-sm font-medium text-gray-900 sm:pl-6 ">
+                        <BsFillFileEarmarkPdfFill className="text-xl text-pink-500" />
                       </td>
-                      <td className='flex mt-3'>
+                      <td className="flex mt-3">
                         <AiOutlineEdit
-                          className='text-xl mt-4'
+                          className="text-xl mt-4"
                           onClick={() => {
                             return router.push(`/edit/${letter.id}`);
                           }}
                         />
                         <button
-                          type='submit'
-                          className='whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6'
+                          type="submit"
+                          className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6"
                           onClick={() => {
                             deleteposts(letter.id);
                             toggle();
                             handleAddAlert();
                           }}
                         >
-                          <BsTrash className='text-xl' />
+                          <BsTrash className="text-xl" />
                         </button>
                       </td>
                     </tr>
