628f335d28c0625dd1292a7200c93c9bb26705cf
Bit-Barron <1bit.baron@gmail.com>
2023-02-08 22:41:41 +0100

    uploadpost

--- web/src/pages/dashboard/letter/uploadpost.tsx
+++ web/src/pages/dashboard/letter/uploadpost.tsx
@@ -1,5 +1,4 @@
-import axios from 'axios';
-import React, { useState } from 'react';
+import React from 'react';
 import { DashboardContainer } from '../../../components/container/DashboardContainer';
 
 interface uploadpostProps {}
