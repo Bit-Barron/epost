f7d8a4e0f670c901fedc77577e94c1af77689cf9
Bit-Barron <1bit.baron@gmail.com>
2023-02-08 14:56:38 +0100

    clean up

--- web/src/pages/edit/[id].tsx
+++ web/src/pages/edit/[id].tsx
@@ -8,8 +8,6 @@ import Checkboxes from '../../components/elements/Checkboxes';
 import Impresum from '../../components/elements/pricepage/Impresum';
 
 const Edit = () => {
-  const [isData, setData] = useState<any[]>([]);
-  const [checked, setChecked] = useState(false);
   const [side, setSide] = useState('');
   const [color, setColor] = useState('');
   const router = useRouter();
@@ -18,7 +16,6 @@ const Edit = () => {
   useEffect(() => {
     const getPosts = async () => {
       const response = await axios.get('/letter/all-user');
-      setData(response.data);
     };
     getPosts();
   }, []);
@@ -28,29 +25,30 @@ const Edit = () => {
       side,
       color,
     });
+    router.push('/dashboard/letter/postbox/postbox');
   };
 
   return (
     <DashboardContainer>
       <div>
-        <h1 className='font-bold text-2xl text-center mt-20 mb-10'>
+        <h1 className="font-bold text-2xl text-center mt-20 mb-10">
           Auftrag {id} bearbeiten
         </h1>
-        <div className='justify-center p-5 flex mt-20'>
-          <div className='bg-[#212323] p-10'>
-            <h1 className='text-secondary font-bold'>Druckeinstellungen</h1>
-            <div className='mt-5 text-gray-500'>
+        <div className="justify-center p-5 flex mt-20">
+          <div className="bg-[#212323] p-10">
+            <h1 className="text-secondary font-bold">Druckeinstellungen</h1>
+            <div className="mt-5 text-gray-500">
               Wählen Sie zwischen einem Schwarzweiß- und Farbdruck <br /> aus
               und geben Sie an, ob Ihre Briefe ein- oder doppelseitig <br />
               gedruckt werden sollen.
             </div>
-            <div className='flex'>
+            <div className="flex">
               <Checkboxes
                 name={'Schwarzweiß'}
                 value={''}
                 onClick={() => setColor('Schwarzweiß')}
               />
-              <div className='ml-20'>
+              <div className="ml-20">
                 <Checkboxes
                   name={'Farbe'}
                   value={''}
@@ -58,13 +56,13 @@ const Edit = () => {
                 />
               </div>
             </div>
-            <div className='flex'>
+            <div className="flex">
               <Checkboxes
                 name={'Einseitig'}
                 onClick={() => setSide('Einseitig')}
                 value={''}
               />
-              <div className='ml-28'>
+              <div className="ml-28">
                 <Checkboxes
                   name={'Doppelseitig'}
                   value={''}
@@ -73,15 +71,15 @@ const Edit = () => {
               </div>
             </div>
           </div>
-          <div className='bg-[#212323] p-10'>
-            <h1 className='text-secondary font-bold'>Versandeinstellungen</h1>
-            <div className='mt-5 text-gray-500 flex'>
-              <AuthInput type={''} label={'Einschreiben'} className='w-60' />
-              <div className='ml-20'>
+          <div className="bg-[#212323] p-10">
+            <h1 className="text-secondary font-bold">Versandeinstellungen</h1>
+            <div className="mt-5 text-gray-500 flex">
+              <AuthInput type={''} label={'Einschreiben'} className="w-60" />
+              <div className="ml-20">
                 <AuthInput
                   type={''}
                   label={'Zeitversetzer Versand '}
-                  className='w-60'
+                  className="w-60"
                 />
               </div>
             </div>
@@ -89,7 +87,7 @@ const Edit = () => {
               name={`C4 Kuvertierhülle: Kuvertierung in einem Umschlag`}
               value={''}
             />
-            <div className='ml-8 text-gray-700'>
+            <div className="ml-8 text-gray-700">
               ohne Falzung (weniger als 9 Blatt)
             </div>
           </div>
@@ -103,7 +101,7 @@ const Edit = () => {
         </div>
       </div>
 
-      <div className='mt-20'>
+      <div className="mt-20">
         <Impresum />
       </div>
     </DashboardContainer>
