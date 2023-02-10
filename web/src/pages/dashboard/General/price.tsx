2b424ad7ef1fdb67baae770cad2e09e2b73c7a9e
Bit-Barron <1bit.baron@gmail.com>
2023-02-10 22:34:14 +0100

    price.tsx

--- web/src/pages/dashboard/General/price.tsx
+++ web/src/pages/dashboard/General/price.tsx
@@ -141,54 +141,54 @@ const tiers = [
 
 function Price() {
   return (
-    <div className='bg-white'>
-      <Popover className='relative bg-white'>
+    <div className="bg-white">
+      <Popover className="relative bg-white">
         <Navbar />
         <Transition
           as={Fragment}
-          enter='duration-200 ease-out'
-          enterFrom='opacity-0 scale-95'
-          enterTo='opacity-100 scale-100'
-          leave='duration-100 ease-in'
-          leaveFrom='opacity-100 scale-100'
-          leaveTo='opacity-0 scale-95'
+          enter="duration-200 ease-out"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="duration-100 ease-in"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
         >
           <Popover.Panel
             focus
-            className='absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden'
+            className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
           >
-            <div className='rounded-lg shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y-2 divide-gray-50'>
-              <div className='pt-5 pb-6 px-5 space-y-6'>
-                <div className='flex items-center justify-between'>
+            <div className="rounded-lg shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y-2 divide-gray-50">
+              <div className="pt-5 pb-6 px-5 space-y-6">
+                <div className="flex items-center justify-between">
                   <div>
                     <Image
                       width={50}
                       height={50}
-                      className='h-8 w-auto'
-                      src='https://tailwindui.com/img/logos/workflow-mark-purple-600.svg'
-                      alt='Workflow'
+                      className="h-8 w-auto"
+                      src="https://tailwindui.com/img/logos/workflow-mark-purple-600.svg"
+                      alt="Workflow"
                     />
                   </div>
-                  <div className='-mr-2'>
-                    <Popover.Button className='bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-purple-500'>
-                      <span className='sr-only'>Close menu</span>
-                      <XIcon className='h-6 w-6' aria-hidden='true' />
+                  <div className="-mr-2">
+                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-purple-500">
+                      <span className="sr-only">Close menu</span>
+                      <XIcon className="h-6 w-6" aria-hidden="true" />
                     </Popover.Button>
                   </div>
                 </div>
-                <div className='mt-6'>
-                  <nav className='grid gap-y-8'>
+                <div className="mt-6">
+                  <nav className="grid gap-y-8">
                     {solutions.map((item, idx) => (
                       <a
                         key={idx}
                         href={item.href}
-                        className='-m-3 p-3 flex items-center rounded-md hover:bg-gray-50'
+                        className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
                       >
                         <item.icon
-                          className='flex-shrink-0 h-6 w-6 text-purple-600'
-                          aria-hidden='true'
+                          className="flex-shrink-0 h-6 w-6 text-purple-600"
+                          aria-hidden="true"
                         />
-                        <span className='ml-3 text-base font-medium text-gray-900'>
+                        <span className="ml-3 text-base font-medium text-gray-900">
                           {item.name}
                         </span>
                       </a>
@@ -196,32 +196,32 @@ function Price() {
                   </nav>
                 </div>
               </div>
-              <div className='py-6 px-5 space-y-6'>
-                <div className='grid grid-cols-2 gap-y-4 gap-x-8'>
+              <div className="py-6 px-5 space-y-6">
+                <div className="grid grid-cols-2 gap-y-4 gap-x-8">
                   <a
-                    href='#'
-                    className='text-base font-medium text-gray-900 hover:text-gray-700'
+                    href="#"
+                    className="text-base font-medium text-gray-900 hover:text-gray-700"
                   >
                     Pricing
                   </a>
 
                   <a
-                    href='#'
-                    className='text-base font-medium text-gray-900 hover:text-gray-700'
+                    href="#"
+                    className="text-base font-medium text-gray-900 hover:text-gray-700"
                   >
                     Docs
                   </a>
 
                   <a
-                    href='#'
-                    className='text-base font-medium text-gray-900 hover:text-gray-700'
+                    href="#"
+                    className="text-base font-medium text-gray-900 hover:text-gray-700"
                   >
                     Blog
                   </a>
 
                   <a
-                    href='#'
-                    className='text-base font-medium text-gray-900 hover:text-gray-700'
+                    href="#"
+                    className="text-base font-medium text-gray-900 hover:text-gray-700"
                   >
                     Contact Sales
                   </a>
@@ -229,24 +229,24 @@ function Price() {
                     <a
                       key={idx}
                       href={item.href}
-                      className='text-base font-medium text-gray-900 hover:text-gray-700'
+                      className="text-base font-medium text-gray-900 hover:text-gray-700"
                     >
                       {item.name}
                     </a>
                   ))}
                 </div>
-                <div className='space-y-6'>
+                <div className="space-y-6">
                   <a
-                    href='#'
-                    className='w-full bg-purple-600 border border-transparent rounded-md py-2 px-4 flex items-center justify-center text-base font-medium text-white hover:bg-purple-700'
+                    href="#"
+                    className="w-full bg-purple-600 border border-transparent rounded-md py-2 px-4 flex items-center justify-center text-base font-medium text-white hover:bg-purple-700"
                   >
                     Sign up
                   </a>
-                  <p className='text-center text-base font-medium text-gray-500'>
+                  <p className="text-center text-base font-medium text-gray-500">
                     Existing customer?
                     <a
-                      href='#'
-                      className='text-purple-600 hover:text-purple-500'
+                      href="#"
+                      className="text-purple-600 hover:text-purple-500"
                     >
                       Sign in
                     </a>
@@ -258,54 +258,54 @@ function Price() {
         </Transition>
       </Popover>
 
-      <div className='bg-gradient-to-b from-white to-gray-50 mx-auto'>
-        <div className='max-w-7xl mx-auto pt-24 px-4 sm:px-6 lg:px-8'>
-          <div className='sm:flex sm:flex-col mr-10 sm:align-center'>
-            <h1 className='text-5xl font-extrabold text-gray-900 sm:text-center'>
+      <div className="bg-gradient-to-b from-white to-gray-50 mx-auto">
+        <div className="max-w-7xl mx-auto pt-24 px-4 sm:px-6 lg:px-8">
+          <div className="sm:flex sm:flex-col mr-10 sm:align-center">
+            <h1 className="text-5xl font-extrabold text-gray-900 sm:text-center">
               Pricing Plans
             </h1>
-            <p className='mt-5 text-xl text-gray-500 sm:text-center'>
+            <p className="mt-5 text-xl text-gray-500 sm:text-center">
               Start building for free, then add a site plan to go live. Account
               plans unlock additional features.
             </p>
           </div>
-          <div className='mt-12 ml-40 flex space-y-2 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-4'>
+          <div className="mt-12 ml-40 flex space-y-2 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-4">
             {tiers.map((tier, idx) => (
               <div
                 key={idx}
-                className='border border-gray-200 rounded-lg shadow-sm divide-y divide-gray-200'
+                className="border border-gray-200 rounded-lg shadow-sm divide-y divide-gray-200"
               >
-                <div className='p-6'>
-                  <h2 className='text-lg leading-6 font-medium text-gray-900'>
+                <div className="p-6">
+                  <h2 className="text-lg leading-6 font-medium text-gray-900">
                     {tier.name}
                   </h2>
-                  <p className='mt-4 text-sm text-gray-500'>
+                  <p className="mt-4 text-sm text-gray-500">
                     {tier.description}
                   </p>
-                  <p className='mt-8'>
-                    <span className='text-4xl font-extrabold text-gray-900'>
+                  <p className="mt-8">
+                    <span className="text-4xl font-extrabold text-gray-900">
                       ${tier.priceMonthly}
                     </span>
                   </p>
                   <a
                     href={tier.href}
-                    className='mt-8 block w-full bg-secondary border border-transparent rounded-md py-2 text-sm font-semibold text-white text-center'
+                    className="mt-8 block w-full bg-secondary border border-transparent rounded-md py-2 text-sm font-semibold text-white text-center"
                   >
                     Buy {tier.name}
                   </a>
                 </div>
-                <div className='pt-6 pb-8 px-6'>
-                  <h3 className='text-xs font-medium text-gray-900 tracking-wide uppercase'>
+                <div className="pt-6 pb-8 px-6">
+                  <h3 className="text-xs font-medium text-gray-900 tracking-wide uppercase">
                     Whats included
                   </h3>
-                  <ul role='list' className='mt-6 space-y-4'>
+                  <ul role="list" className="mt-6 space-y-4">
                     {tier.features.map((feature) => (
-                      <li key={feature} className='flex space-x-3'>
+                      <li key={feature} className="flex space-x-3">
                         <CheckIconSolid
-                          className='flex-shrink-0 h-5 w-5 text-green-500'
-                          aria-hidden='true'
+                          className="flex-shrink-0 h-5 w-5 text-green-500"
+                          aria-hidden="true"
                         />
-                        <span className='text-sm text-gray-500'>{feature}</span>
+                        <span className="text-sm text-gray-500">{feature}</span>
                       </li>
                     ))}
                   </ul>
@@ -317,7 +317,7 @@ function Price() {
         <AllInOne />
       </div>
 
-      <div className='bg-secondary'>
+      <div className="bg-secondary">
         <Partnership />
       </div>
 
