f1e3acd675e43f3e6b63338e88d2e86787a56389
hToWnKiLLeR <rico47198@live.de>
2023-02-10 16:23:22 +0100

    test

--- web/src/components/elements/Navbar.tsx
+++ web/src/components/elements/Navbar.tsx
@@ -112,7 +112,7 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
             <li>
               <a
                 href='#'
-                className='block py-2 pl-3 text-xl pr-4 text-gray-700 bg-main rounded md:bg-transparent md:p-0 '
+                className='block py-2 pl-3 text-xl pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
                 aria-current='page'
               >
                 Brief Übertragen
