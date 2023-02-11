fcd73074b2a35ac469ca24ae9b70cc5618b0c909
Bit-Barron <1bit.baron@gmail.com>
2023-02-11 21:43:14 +0100

    username

--- web/src/components/container/Admin/AdminDesktopSidebar.tsx
+++ web/src/components/container/Admin/AdminDesktopSidebar.tsx
@@ -29,7 +29,7 @@ const user = [
   },
   {
     name: 'Userverwaltung',
-    href: '#',
+    href: '/admin/user/usermanagment',
     icon: AiOutlineArrowRight,
     current: false,
   },
@@ -47,94 +47,94 @@ const AdminDesktopSidebar: React.FC<AdminDesktopSidebarProps> = ({
   children,
 }) => {
   return (
-    <div className='hidden lg:flex lg:flex-col lg:w-64 lg:fixed lg:inset-y-0 lg:border-r lg:border-gray-200 lg:pt-5 lg:pb-4 lg:bg-gray-100'>
+    <div className="hidden lg:flex lg:flex-col lg:w-64 lg:fixed lg:inset-y-0 lg:border-r lg:border-gray-200 lg:pt-5 lg:pb-4 lg:bg-gray-100">
       <div>
-        <div className='flex'>
-          <Image src={main} alt='main' width={70} height={70} />
-          <div className='font-bold text-2xl mt-4'>Postshield</div>
+        <div className="flex">
+          <Image src={main} alt="main" width={70} height={70} />
+          <div className="font-bold text-2xl mt-4">Postshield</div>
         </div>
-        <nav className='px-3'>
+        <nav className="px-3">
           <h3
-            className='px-3 text-xs font-semibold text-gray-500 uppercase tracking-wider'
-            id='desktop-teams-headline'
+            className="px-3 text-xs font-semibold text-gray-500 uppercase tracking-wider"
+            id="desktop-teams-headline"
           >
             Post
           </h3>
-          <div className='space-y-1'>
+          <div className="space-y-1">
             {navigation.map((item) => (
               <Link
                 key={item.name}
                 href={item.href}
                 className={classNames(
                   item.current ? '' : '',
-                  'group flex items-center hover:bg-secondary px-2 py-1 text-sm font-semibold hover:text-gray-900 '
+                  'group flex items-center hover:bg-secondary px-2 py-1 text-sm font-semibold hover:text-gray-900 ',
                 )}
                 aria-current={item.current ? 'page' : undefined}
               >
                 <item.icon
                   className={classNames(
                     item.current ? 'text-secondary' : 'text-secondary',
-                    'mr-3 flex-shrink-0 h-5 w-6'
+                    'mr-3 flex-shrink-0 h-5 w-6',
                   )}
-                  aria-hidden='true'
+                  aria-hidden="true"
                 />
-                <div className=''>{item.name}</div>
+                <div className="">{item.name}</div>
               </Link>
             ))}
           </div>
           <h3
-            className='px-3 mt-5 text-xs font-semibold text-gray-500 uppercase tracking-wider'
-            id='desktop-teams-headline'
+            className="px-3 mt-5 text-xs font-semibold text-gray-500 uppercase tracking-wider"
+            id="desktop-teams-headline"
           >
             User
           </h3>
-          <div className='space-y-1'>
+          <div className="space-y-1">
             {user.map((item) => (
               <Link
                 key={item.name}
                 href={item.href}
                 className={classNames(
                   item.current ? '' : '',
-                  'group flex items-center hover:bg-secondary px-2 py-1 text-sm font-semibold hover:text-gray-900 '
+                  'group flex items-center hover:bg-secondary px-2 py-1 text-sm font-semibold hover:text-gray-900 ',
                 )}
                 aria-current={item.current ? 'page' : undefined}
               >
                 <item.icon
                   className={classNames(
                     item.current ? 'text-secondary' : 'text-secondary',
-                    'mr-3 flex-shrink-0 h-5 w-6'
+                    'mr-3 flex-shrink-0 h-5 w-6',
                   )}
-                  aria-hidden='true'
+                  aria-hidden="true"
                 />
-                <div className=''>{item.name}</div>
+                <div className="">{item.name}</div>
               </Link>
             ))}
           </div>
 
-          <div className='mt-8'></div>
-          <div className='mt-8'>
+          <div className="mt-8"></div>
+          <div className="mt-8">
             <h3
-              className='px-3 text-xs font-semibold text-gray-500 uppercase tracking-wider'
-              id='desktop-teams-headline'
+              className="px-3 text-xs font-semibold text-gray-500 uppercase tracking-wider"
+              id="desktop-teams-headline"
             >
               SHIELDPOST.DE
             </h3>
-            <div className='mt-2 mb-10'>
+            <div className="mt-2 mb-10">
               {shieldPost.map((team) => (
                 <a
                   key={team.name}
                   href={team.href}
-                  className='group flex items-center mt-2 hover:bg-secondary px-2 py-1 text-sm font-semibold hover:text-gray-900 '
+                  className="group flex items-center mt-2 hover:bg-secondary px-2 py-1 text-sm font-semibold hover:text-gray-900 "
                 >
                   <team.icon
                     className={classNames(
                       team.current ? 'text-secondary' : 'text-secondary',
-                      'mr-3 flex-shrink-0 h-5 w-6'
+                      'mr-3 flex-shrink-0 h-5 w-6',
                     )}
-                    aria-hidden='true'
+                    aria-hidden="true"
                   />
-                  <span aria-hidden='true' />
-                  <span className='truncate'>{team.name}</span>
+                  <span aria-hidden="true" />
+                  <span className="truncate">{team.name}</span>
                 </a>
               ))}
             </div>
--- web/src/pages/admin/user/usermanagment.tsx
+++ web/src/pages/admin/user/usermanagment.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import AdminContainer from '../../../components/container/AdminContainer';
 
 interface usermanagmentProps {}
 
 const usermanagment: React.FC<usermanagmentProps> = ({}) => {
-  return <div>asdasdjmasdlöäkj</div>;
+  return (
+    <AdminContainer>
+      <div>Usermanagment</div>
+    </AdminContainer>
+  );
 };
 
 export default usermanagment;
