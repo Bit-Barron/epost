efd1475605a93f869b140a8f11df109addbbd8d5
Bit-Barron <1bit.baron@gmail.com>
2023-02-11 21:40:11 +0100

    usermanagment

---
+++ web/src/pages/admin/user/usermanagment.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface usermanagmentProps {}
+
+const usermanagment: React.FC<usermanagmentProps> = ({}) => {
+  return <div>asdasdjmasdlöäkj</div>;
+};
+
+export default usermanagment;
